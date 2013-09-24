d8a5a2f09edcee57a1091ed256935cd8181bed04
Florian <captain.hulog@gmail.com>
2013-09-24 19:52:35 +0200

    ksjdf

--- Web/public_html/Veranstaltung03/mail.php
+++ Web/public_html/Veranstaltung03/mail.php
@@ -1,7 +1,7 @@
 <?php
    
     $headers = "From: <luethifl@students.zhaw.ch>\r\n";
-    $headers .= "Reply-To: Michael Hoehn <luethifl@students.zhaw.ch>\r\n";
+    $headers .= "Reply-To: Niemand <luethifl@students.zhaw.ch>\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
     
