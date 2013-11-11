33fa7fcf6775ee6a1c187e5fd489334f4ab3871c
Florian <captain.hulog@gmail.com>
2013-11-11 19:49:52 +0100

    u9

---
+++ Web/public_html/Veranstaltung09/temo/protected/views/message/goodbye.php
@@ -0,0 +1,7 @@
+<h1>Ciao Yii developer!</h1>
+
+<p>
+<?php
+	echo CHtml::link("Hello",array('message/helloWorld'));
+?>
+</p>
---
+++ Web/public_html/Veranstaltung09/temo/protected/views/message/helloWorld.php
@@ -0,0 +1,17 @@
+<?php
+
+$this->breadcrumbs = array (
+	'Message'=>array('message/index'),
+	'HelloWorld',
+);
+
+?>
+
+<h1>Hello, Wörld!</h1>
+<h3><?php echo $time; ?></h3>
+
+<p>
+<?php
+	echo CHtml::link("Goodbye",array('message/goodbye'));
+?>
+</p>
